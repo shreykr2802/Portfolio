462223ddc164516d7e10ea927cd089b39e4d200d
Shrey Kumar <>
2022-01-30 23:07:16 +0530

    component folder structure fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import { Provider } from 'react-redux'
 import store from '../redux/store'
 import styles from '../styles/Home.module.scss'
-import HomeContents from './homeContents/HomeContents'
+import HomeContents from '../components/about/homeContents/HomeContents'
 
 const Home: NextPage = () => {
   return (
