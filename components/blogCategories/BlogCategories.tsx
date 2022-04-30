f3995e1648d9bef0071a30c2a0073b6846c39057
Shrey Kumar <>
2022-05-01 02:00:38 +0530

    Admin Portal Changes and RWD

--- components/blogCategories/BlogCategories.tsx
+++ components/blogCategories/BlogCategories.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import { useDispatch } from "react-redux";
+import { fetchBlogTagsDataStart } from "../../redux/slices/blogSlice";
 import useSelector from "../../utils/useSelector";
 import classes from "./BlogCategories.module.scss";
 
@@ -6,12 +8,14 @@ const BlogCategories = () => {
 
     const categories = useSelector(state => state.tags.tags);
 
+    const dispatch = useDispatch();
+
     return (
         <>
             <div className={classes['blog-categories']}>
                 {
                     categories.map((category) => (
-                        <div className={classes['category-box']} key={category.id}>
+                        <div className={classes['category-box']} key={category.id} onClick={() => dispatch(fetchBlogTagsDataStart(category.id))}>
                             <p className={classes['category-name']}>{category.name}</p>
                             <div className={classes['category-image-box']}>
                                 <Image src={category.data} layout="fill" className={classes['category-image']} />
--- components/blogsContainer/BlogsContainer.tsx
+++ components/blogsContainer/BlogsContainer.tsx
@@ -1,12 +1,25 @@
+import useSelector from "../../utils/useSelector";
 import BlogCategories from "../blogCategories/BlogCategories";
 import BlogsSmallInfoList from "../blogsSmallInfoList/BlogsSmallInfoList";
+import Loading from "../loading/Loading";
 import classes from "./BlogsContainer.module.scss";
 
 const BlogsContainer = () => {
+
+    const blogsLoading = useSelector(state => state.blogs.loading);
+    const tagsLoading = useSelector(state => state.tags.loading)
+
     return (
         <div className={classes['blog-container']}>
-            <BlogsSmallInfoList />
-            <BlogCategories />
+            {(!blogsLoading && !tagsLoading) ? (
+                <>
+                    <BlogsSmallInfoList />
+                    <BlogCategories />
+                </>
+            ) : (
+                <Loading />
+            )}
+
         </div>
     );
 }
